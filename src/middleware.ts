43686c07053a6e9a4285159c0a1c70ce1071f68b
RONNIE NYAMHUTE <ronnienyamhute81@gmail.com>
2025-08-12 15:37:19 +0200

    chnaged

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,26 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  // These are the pages everyone can see, even if they are not signed in.
-  publicRoutes: [
-    '/',
-    '/sign-in(.*)',
-    '/sign-up(.*)'
-  ],
+// Define the routes that are publicly accessible.
+// Everyone can see these pages, even if they are not signed in.
+const isPublicRoute = createRouteMatcher([
+  '/', // The landing page
+  '/sign-in(.*)', // The sign-in pages
+  '/sign-up(.*)', // The sign-up pages
+  '/api/(.*)', // Any API routes you have
+]);
+
+export default clerkMiddleware((auth, req) => {
+  // If the route is not public, then protect it.
+  // Clerk will handle the redirection to the sign-in page.
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
 });
 
 export const config = {
-  // This makes sure the middleware runs on all pages except for static files.
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  // This matcher ensures the middleware runs on all routes except for static files.
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
