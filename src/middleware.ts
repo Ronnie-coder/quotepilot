23eeb303604b26f91d972b3087e48deaff5f6c7e
RONNIE NYAMHUTE <ronnienyamhute81@gmail.com>
2025-08-12 15:42:17 +0200

    middle

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Define the routes that are publicly accessible.
-// Everyone can see these pages, even if they are not signed in.
-const isPublicRoute = createRouteMatcher([
-  '/', // The landing page
-  '/sign-in(.*)', // The sign-in pages
-  '/sign-up(.*)', // The sign-up pages
-  '/api/(.*)', // Any API routes you have
-]);
-
-export default clerkMiddleware((auth, req) => {
-  // If the route is not public, then protect it.
-  // Clerk will handle the redirection to the sign-in page.
-  if (!isPublicRoute(req)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   // This matcher ensures the middleware runs on all routes except for static files.
